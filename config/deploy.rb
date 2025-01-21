0e6e1d8b6c9245dbf703cc9adf6ac1c2e0d7cf64
tommyoguro <tomoharu.oguro.2022@globis.ac.jp>
2025-01-21 19:48:50 +0900

    init

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,6 +7,8 @@ set :bundle_without, %w{test}.join(':')
 
 set :rbenv_version, '3.3.0'
 
+set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_version)} RBENV_VERSION=#{fetch(:rbenv_version)} #{fetch(:rbenv_version)}/bin/rbenv exec"
+
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
